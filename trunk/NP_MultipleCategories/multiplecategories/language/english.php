28f5f8a30588b807f2dda23da0e6c8c1c7386242
satona <satona@1ca29b6e-896d-4ea0-84a5-967f57386b96>
2006-10-03 12:19:35 +0000

    (none)
    git-svn-id: https://svn.sourceforge.jp/svnroot/nucleus-jp/plugin@407 1ca29b6e-896d-4ea0-84a5-967f57386b96

---
+++ trunk/NP_MultipleCategories/multiplecategories/language/english.php
@@ -0,0 +1,33 @@
+<?php 
+define('_MC_EDIT_PLUGIN_OPTIONS',			'Edit Plugin Options'); 
+define('_MC_SUB_CATEGORIES',				'Sub Categories'); 
+define('_MC_EDIT_SUB_CATEGORIES',			'Edit sub categories'); 
+define('_MC_EDIT_SUB_CATEGORIES_OF',		'Edit sub categories of'); 
+define('_MC_CREATE_NEW_SUB_CATEGORY',		'Create New Sub Category'); 
+define('_MC_SCAT_NAME',						'Name');
+define('_MC_SCAT_DESC',						'Description');
+define('_MC_SCAT_CREATE',					'Create Sub Category');
+define('_MC_SCAT_UPDATE',					'UPDATE Sub Category');
+define('_MC_SCAT_EDIT',						'Edit');
+define('_MC_SCAT_DATA_UPDATE',				'Sub category data has been saved.');
+define('_MC_SCAT_MISSING',					'Sub category is missing...');
+define('_MC_SCAT_ERROR_NAME',					'Error! Input a name.');
+define('_MC_CONFIRMTXT_SCAT',			'The sub category you are going to delete is: ');
+
+define('_MC_SCAT_PARENT_NAME',				'Parent');
+define('_MC_SCAT_ORDER_UPDATE',				'Sub category order has been updated.');
+define('_MC_MODIFY_CHILDREN_ORDER',				'Modify Children Order');
+define('_MC_SUBMIT_CHILDREN_ORDER',				'Submit Children Order');
+define('_MC_NO_CHILDREN_ORDER',				'( No child categories are here )');
+define('_MC_SCAT_DELETE_NOTE_LIST',				'This subcategory you\'re going to delete has child(ren) listed below.');
+define('_MC_SCAT_DELETE_NOTE_PARENT',				'If delete, these subcategoris will be automatically belong to');
+define('_MC_SCAT_TABLE_UPDATE_INFO',				'If you want to use UNLIMITED DEPTH SUBCATEGORY, table upgrade is required.');
+define('_MC_SCAT_TABLE_UPDATE',				'upgrade successed.');
+
+define('_MC_SCAT_PARENT_NAME_DESC',				"iWhen oneself is specified, it doesn't change.j");
+define('_MC_SHOW_ORDER_MENU_KEY',				'sort key');
+define('_MC_SHOW_ORDER_MENU_SNAME',				'sname');
+define('_MC_SHOW_ORDER_MENU_SDESC',				'sdesc');
+define('_MC_SHOW_ORDER_MENU_INDIVIDUAL',				'individual');
+
+?>
---
+++ trunk/NP_MultipleCategories/multiplecategories/language/japanese-utf8.php
@@ -0,0 +1,33 @@
+<?php 
+define('_MC_EDIT_PLUGIN_OPTIONS',			'このプラグインのオプション編集画面へ'); 
+define('_MC_SUB_CATEGORIES',				'サブカテゴリ'); 
+define('_MC_EDIT_SUB_CATEGORIES',			'サブカテゴリの編集画面へ'); 
+define('_MC_EDIT_SUB_CATEGORIES_OF',		'サブカテゴリの編集 - カテゴリ名'); 
+define('_MC_CREATE_NEW_SUB_CATEGORY',		'サブカテゴリの新規作成'); 
+define('_MC_SCAT_NAME',						'サブカテゴリ名');
+define('_MC_SCAT_DESC',						'サブカテゴリの説明');
+define('_MC_SCAT_CREATE',					'新しいサブカテゴリを作成');
+define('_MC_SCAT_UPDATE',					'サブカテゴリの更新');
+define('_MC_SCAT_EDIT',						'サブカテゴリの編集 - サブカテゴリ名 ');
+define('_MC_SCAT_DATA_UPDATE',				'サブカテゴリのデータを更新しました。');
+define('_MC_SCAT_MISSING',					'サブカテゴリが見つかりません。');
+define('_MC_SCAT_ERROR_NAME',				'エラーです。サブカテゴリ名を入力してください。');
+define('_MC_CONFIRMTXT_SCAT',			'以下のサブカテゴリを削除しようとしています: ');
+
+define('_MC_SCAT_PARENT_NAME',				'親となる(サブ)カテゴリ');
+define('_MC_SCAT_ORDER_UPDATE',				'サブカテゴリの並び順を更新しました。');
+define('_MC_MODIFY_CHILDREN_ORDER',				'子サブカテゴリの並び順の変更');
+define('_MC_SUBMIT_CHILDREN_ORDER',				'並び順を反映');
+define('_MC_NO_CHILDREN_ORDER',				'( 子となるサブカテゴリは複数ありません )');
+define('_MC_SCAT_DELETE_NOTE_LIST',				'削除しようとするサブカテゴリには、直下に以下の子サブカテゴリが存在します');
+define('_MC_SCAT_DELETE_NOTE_PARENT',				'もし削除を実行した場合、子カテゴリは階層が繰り上がり次の(サブ)カテゴリに自動的に属します');
+define('_MC_SCAT_TABLE_UPDATE_INFO',				'サブカテゴリを無限階層管理したい場合は、テーブルのアップグレードが必要です');
+define('_MC_SCAT_TABLE_UPDATE',				'サブカテゴリ管理用テーブルのアップグレードは完了しました');
+
+define('_MC_SCAT_PARENT_NAME_DESC',				'（自分自身を指定した場合、変更しません。）');
+define('_MC_SHOW_ORDER_MENU_KEY',				'並び替え基準');
+define('_MC_SHOW_ORDER_MENU_SNAME',				'名前');
+define('_MC_SHOW_ORDER_MENU_SDESC',				'説明');
+define('_MC_SHOW_ORDER_MENU_INDIVIDUAL',				'個別指定');
+
+?>
