ed3e63e2d6fca783a7885c94407a146ffabb28c2
shizuki <shizuki@1ca29b6e-896d-4ea0-84a5-967f57386b96>
2006-10-09 14:21:51 +0000

    言語ファイル差し替え
    git-svn-id: https://svn.sourceforge.jp/svnroot/nucleus-jp/plugin@438 1ca29b6e-896d-4ea0-84a5-967f57386b96

--- trunk/NP_CustomURL/customurl/language/english.php
+++ trunk/NP_CustomURL/customurl/language/english.php
@@ -1,34 +1,48 @@
 <?php
 
-//PLUGIN MESSAGES
+// PLUGIN MESSAGES
+
+// Plugin description
 	define('_DESCRIPTION',			'This plugin generates the static link from website URL created by NucleusCMS.');
-	define('_OP_TABLE_DELETE',		'Drop tables on uninstall ?');
-	define('_OP_QUICK_LINK',		'Show in Quick Menu ?');
-	define('_OP_ITEM_PATH',			'Plugin request URI');
-	define('_OP_BLOG_PATH',			'Weblog request URI');
-	define('_OP_DEF_ITEM_KEY',		'Default prefix of Item request URI');
-	define('_OP_DEF_CAT_KEY',		'Default prefix of Categories request URI');
-	define('_OP_DEF_SCAT_KEY',		'Default prefix of Subcategories request URI');
-	define('_OP_USE_CURL',			'Use URI alias in this weblog');
-	define('_OP_CATEGORY_PATH',		'Category request URI');
-	define('_OP_MEMBER_PATH',		'Member request URI');
+
+// Plugin Options
+// Global
 	define('_OP_ARCHIVE_DIR_NAME',	'Archive directory request URI');
 	define('_OP_ARCHIVES_DIR_NAME',	'Archives directory request URI');
 	define('_OP_MEMBER_DIR_NAME',	'Member directory request URI');
+	define('_OP_DEF_ITEM_KEY',		'Default prefix of Item request URI');
+	define('_OP_DEF_CAT_KEY',		'Default prefix of Categories request URI');
+	define('_OP_DEF_SCAT_KEY',		'Default prefix of Subcategories request URI');
+	define('_OP_NOT_FOUND',			'Behavior when there were not data to fall under');
+	define('_OP_TABLE_DELETE',		'Drop tables on uninstall ?');
+	define('_OP_QUICK_LINK',		'Show in Quick Menu ?');
+// Blog
+	define('_OP_USE_CURL',			'Use URI alias in this weblog');
+	define('_OP_BLOG_PATH',			'Weblog request URI');
+	define('_OP_RED_NORM',			'Redirect when nomal URI (/index.php?itemid=123 to /item_123.html)');
+	define('_OP_RED_SEARCH',		'Redirect when search (/index.php?query=word to /search/word)');
+// Item
+	define('_OP_ITEM_PATH',			'Plugin request URI');
+// Category
+	define('_OP_CATEGORY_PATH',		'Category request URI');
+// Member
+	define('_OP_MEMBER_PATH',		'Member request URI');
+
+// Error messages
+	define('_NO_SUCH_URI',			'Unable to connect requested URI.<br /> Please check URI and try again.<br />');
 	define('_INVALID_ERROR',		'<h2>Invalid path</h2>');
 	define('_INVALID_MSG',			'Invalid caracter is included.<br /> Available characters are only  [A-Za-z0-9/-(hyphen)/_(underscore)]<br />And also extention is not allowed like [.html]');
 	define('_CONFLICT_ERROR',		'<h2>Specified URI already exists.</h2>');
 	define('_CONFLICT_MSG',			'Special id is automatically added at the end of URI because specified URI already exists in this weblog.<br /> Please edit again if you want to chage URI.');
 	define('_DELETE_PATH',			'<h2>Alias was successfully deleted.</h2>');
 	define('_DELETE_MSG',			'Drop registered URI because of empty setting.<br /> Nomal FancyURL is applied for the link URI.');
-	define('_NO_SUCH_URI',			'Unable to connect requested URI.<br /> Please check URI and try again.<br />
-Requested URI may exist in the other weblogs.<br />
-In case「/category_12/item_123.html」 url type does not work, try 「/category/12/item/123」. The change may load the contents.');
 	define('_NOT_VALID_BLOG',		'Specified weblog does not exist.');
 	define('_NOT_VALID_ITEM',		'Specified item does not exist.');
 	define('_NOT_VALID_CAT',		'Specified category does not exist.');
 	define('_NOT_VALID_SUBCAT',		'Specified subcategory does not exist.');
 	define('_NOT_VALID_MEMBER',		'Specified member does not exist.');
+
+// Admin area
 	define('_ADMIN_TITLE',			'URL CUSTOMIZE');
 	define('_QUICK_TIPS',			'Manage link URI');
 	define('_ERROR_DISALLOWED',		'Access denied.');
