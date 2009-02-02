a9a2b2bad3cc3e5390f940793ba9a36525fbeb0e
shizuki <shizuki@1ca29b6e-896d-4ea0-84a5-967f57386b96>
2009-02-02 05:20:19 +0000

    git-svn-id: https://svn.sourceforge.jp/svnroot/nucleus-jp/plugin@773 1ca29b6e-896d-4ea0-84a5-967f57386b96

--- trunk/NP_GoogleSitemap/googlesitemap/english.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define('_G_SITEMAP_DESC',		"This plugin provides a Google sitemap for your website.<br />" .
-									"Sitemap URL: http://example.cm/sitemap.xml");
-	define('_G_SITEMAP_ALLB',		"Generate All Blog's Google Sitemap");
-	define('_G_SITEMAP_PING',		"Ping Google after adding a new item");
-	define('_G_SITEMAP_INC',		"Include this blog in Google Sitemap when All Blog mode");
-	define('_G_SITEMAP_PCSM',		"Virtual file name for PC Sitemap");
-	define('_G_SITEMAP_MBSM',		"Virtual file name for Mobile Sitemap");
-?>
--- trunk/NP_RejectASCIIComment/rejectasciicomment/japanese.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define('_REJECT_ASCII_DESCRIPTION', "日本語を含まないコメント・トラックバックをスパムとして処理します");
-	define('_REJECT_ASCII_HOOK',        "日本語を含まないコメント・トラックバックをスパムとして処理しますか？");
-	define('_REJECT_ASCII_DELTB',       "日本語を含まないトラックバックをデータベースに登録せずに破棄しますか？");
-	define('_REJECT_ASCII_COMMENT',     "への コメント をスパムとして処理しました 内容(");
-	define('_REJECT_ASCII_TRACKBACK',   "への トラックバック をスパムとして処理しました 内容(");
-	define('_REJECT_ASCII_INFOHEAD',    "アイテムID ");
