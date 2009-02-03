1675390e66ca5687954e80d990c9527baa81c8ba
shizuki <shizuki@1ca29b6e-896d-4ea0-84a5-967f57386b96>
2009-02-03 00:08:25 +0000

    git-svn-id: https://svn.sourceforge.jp/svnroot/nucleus-jp/plugin@785 1ca29b6e-896d-4ea0-84a5-967f57386b96

---
+++ NP_Dtree/trunk/dtree/language/english.php
@@ -0,0 +1,13 @@
+<?php
+	define('_DTREE_DESCRIPTION',	'Show Navigation Tree. Usage: &lt;%Dtree()%&gt;');
+	define('_DTREE_DIR_LINK',		'Should folders be links.');
+	define('_DTREE_SELECTION',	'Nodes can be selected(highlighted).');
+	define('_DTREE_COOKIE',		'The tree uses cookies to rember it\'s state.');
+	define('_DTREE_LINE',			'Tree is drawn with lines.');
+	define('_DTREE_ICON',			'Tree is drawn with icons.');
+	define('_DTREE_ST_TEXT',		'Displays node names in the statusbar instead of the url.');
+	define('_DTREE_CL_SLEVEL',	'Only one node within a parent can be expanded at the same time. [Open All] and [Close All] functions do not work when this is enabled.');
+	define('_DTREE_IN_ORDER',		'If parent nodes are always added before children, setting this to true speeds up the tree.');
+	define('_DTREE_OPENALL',		'Open All');
+	define('_DTREE_CLOSEALL',		'Close All');
+?>
