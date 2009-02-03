eb40308f4e8041b0b6ebd8ac92294198e4d6002d
shizuki <shizuki@1ca29b6e-896d-4ea0-84a5-967f57386b96>
2009-02-03 00:11:43 +0000

    git-svn-id: https://svn.sourceforge.jp/svnroot/nucleus-jp/plugin@798 1ca29b6e-896d-4ea0-84a5-967f57386b96

---
+++ NP_SEOSitemaps/tags/seositemaps/english.php
@@ -0,0 +1,14 @@
+<?php
+	define('_G_SITEMAP_DESC',   "This plugin provides a Google sitemap for your website." .
+								"Sitemap URL: http://example.cm/sitemap.xml");
+	define('_G_SITEMAP_ALLB',   "Generate All Blog's Google Sitemap");
+	define('_G_SITEMAP_PING_G', "Ping Google after adding a new item");
+	define('_G_SITEMAP_PING_Y', "Ping Yahoo! after adding a new item");
+	define('_G_SITEMAP_PING_L', "Ping Live Search after adding a new item");
+//	define('_G_SITEMAP_YAPID',  "Yahoo! Application ID");
+	define('_G_SITEMAP_INC',    "Include this blog in Google Sitemap when All Blog mode");
+	define('_G_SITEMAP_PCSM',   "Virtual file name for PC Sitemap");
+	define('_G_SITEMAP_MBSM',   "Virtual file name for Mobile Sitemap");
+	define('_G_SITEMAP_BPRI',   "Priority of BLOG");
+	define('_G_SITEMAP_CPRI',   "Priority of CATEGORY");
+	define('_G_SITEMAP_IPRI',   "Priority of ITEM");
---
+++ NP_SEOSitemaps/tags/seositemaps/japanese-utf8.php
@@ -0,0 +1,14 @@
+<?php
+	define('_G_SITEMAP_DESC',   "「Googleウェブマスターツール」用のサイトマップを作成します<br />" .
+								"Sitemap URL: http://example.cm/sitemap.xml");
+	define('_G_SITEMAP_ALLB',   "AllBlog モードでサイトマップを作成する(サブ/マルチドメインは除外)");
+	define('_G_SITEMAP_PING_G', "アイテム追加時に Google に更新を通知する");
+	define('_G_SITEMAP_PING_Y', "アイテム追加時に Yahoo! に更新を通知する");
+	define('_G_SITEMAP_PING_L', "アイテム追加時に Live Search に更新を通知する");
+//	define('_G_SITEMAP_YAPID',  "Yahoo! Application ID");
+	define('_G_SITEMAP_INC',    "AllBlog モードのときにこのブログを含める");
+	define('_G_SITEMAP_PCSM',   "PC 用サイトマップファイルの仮想ファイル名");
+	define('_G_SITEMAP_MBSM',   "モバイル用サイトマップファイルの仮想ファイル名");
+	define('_G_SITEMAP_BPRI',   "ブログの Priority 値");
+	define('_G_SITEMAP_CPRI',   "カテゴリーの Priority 値");
+	define('_G_SITEMAP_IPRI',   "アイテムの Priority 値");
