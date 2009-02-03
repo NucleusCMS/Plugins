fc39d406408b9530d154deed0b0bb2cbe92e10a5
shizuki <shizuki@1ca29b6e-896d-4ea0-84a5-967f57386b96>
2009-02-03 00:09:29 +0000

    git-svn-id: https://svn.sourceforge.jp/svnroot/nucleus-jp/plugin@788 1ca29b6e-896d-4ea0-84a5-967f57386b96

---
+++ NP_LatestWritebacks/tags/latestwritebacks/english.php
@@ -0,0 +1,13 @@
+<?php
+
+	define('_NP_LRWITEBACKS_DESC',  'This plugin can be used to display the last few comments and Trackbacks.  '
+								  . 'Usage:&lt;%LatestWritebacks(10,current,all)%&gt;');
+	define('_NP_LRWITEBACKS_TZLOC', 'Locale');
+	define('_NP_LRWITEBACKS_CDFMT', 'Date format(for Comments)');
+	define('_NP_LRWITEBACKS_TEFMT', 'Date format(for Trackbacks)');
+	define('_NP_LRWITEBACKS_CHEAD', '[Comment list]Header');
+	define('_NP_LRWITEBACKS_CBODY', '[Comment list]Body');
+	define('_NP_LRWITEBACKS_CFOOT', '[Comment list]Footer');
+	define('_NP_LRWITEBACKS_THEAD', '[Trackback list]Header');
+	define('_NP_LRWITEBACKS_TBODY', '[Trackback list]Body');
+	define('_NP_LRWITEBACKS_TFOOT', '[Trackback list]Footer');
---
+++ NP_LatestWritebacks/tags/latestwritebacks/japanese-utf8.php
@@ -0,0 +1,13 @@
+<?php
+
+	define('_NP_LRWITEBACKS_DESC',  '最新のコメント/トラックバックを、パラメーターで指定した数だけ表示します<br />'
+								  . '書式：&lt;%LatestWritebacks(10,current,all)%&gt;');
+	define('_NP_LRWITEBACKS_TZLOC', 'タイムゾーンのロケール');
+	define('_NP_LRWITEBACKS_CDFMT', '時間と日付のフォーマット(コメント用)');
+	define('_NP_LRWITEBACKS_TEFMT', '時間と日付のフォーマット(トラックバック用)');
+	define('_NP_LRWITEBACKS_CHEAD', 'コメントリストのヘッダ');
+	define('_NP_LRWITEBACKS_CBODY', 'コメントリストの本体');
+	define('_NP_LRWITEBACKS_CFOOT', 'コメントリストのフッタ');
+	define('_NP_LRWITEBACKS_THEAD', 'トラックバックリストのヘッダ');
+	define('_NP_LRWITEBACKS_TBODY', 'トラックバックリストの本体');
+	define('_NP_LRWITEBACKS_TFOOT', 'トラックバックリストのフッタ');
